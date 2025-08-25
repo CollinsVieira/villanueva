094c0816561c576390fa3f3b01a95ca921ecd22e
CollinsVieira <collins000000000@gmail.com>
2025-08-25 17:49:19 -0500

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://192.168.*:8000',
+        target: 'http://192.168.100.85:8000',
         changeOrigin: true,
         secure: false,
       },
