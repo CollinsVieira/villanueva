35a27ded682f7bb8773342e87769bf89452fc038
CollinsVieira <collins000000000@gmail.com>
2025-08-26 13:47:36 -0500

    bg-black remove

--- frontend/src/components/Lotes/LoteDetailModal.tsx
+++ frontend/src/components/Lotes/LoteDetailModal.tsx
@@ -31,7 +31,7 @@ const LoteDetailModal: React.FC<LoteDetailModalProps> = ({ loteId, onClose }) =>
 
   if (isLoading || !lote) {
     return (
-      <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+      <div className="fixed inset-0 bg-opacity-50 flex items-center justify-center z-50">
         <LoadingSpinner />
       </div>
     );
